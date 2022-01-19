78778906c5188b54a7a3e2bab3ba4de75688408c
PedroAugustoDev <74946703+PedroAugustoDev@users.noreply.github.com>
2022-01-18 22:05:18 -0300

    first update in 2022

--- src/App.java
+++ src/App.java
@@ -1,6 +1,19 @@
+import Colecoes.Color;
+import Colecoes.Pilha;
+
 public class App {
   
   public static void main(String[] args) {
-    
+    final Pilha stack = new Pilha();
+    stack
+      .push(new Color("Blue"))
+      .push(new Color("Red"))
+      .push(new Color("Yellow"));
+
+    System.out.println("Desempilhando a pilha de cores:");
+    System.out.println("A última cor a entrar na pilha foi: " + stack.pop().getColor());
+    System.out.println("A penultima cor a entrar na pilha foi: " + stack.pop().getColor());
+    System.out.println("A primeira cor a entrar na pilha foi: " + stack.pop().getColor());
+
   }
 }
